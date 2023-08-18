f8cacfb8afd471eb9037fc93ecb093195c3c7cd6
VICTOR DAVI <113daviiicamach@gmail.com>
2023-08-18 20:34:43 -0300

    add

--- javascript/aula-16/ambiente.js
+++ javascript/aula-16/ambiente.js
@@ -1,21 +1,44 @@
-function regreess(n){ // (n) Parâmetro
+// function regreess(n){ // (n) Parâmetro
 
-    console.log(n) // {bloco} - Ação  
+//     console.log(n) // {bloco} - Ação  
 
-    let nextNumber = n - 1
+//     let nextNumber = n - 1
     
+//     if(nextNumber > 0){
+//         regreess(nextNumber) //chamada / return a própria função
+//     }
+//     else
+//     'ERROR' 
+
+// }
+
+// regreess(4)
+
+// let newYearCountDown = regreess
+
+// regreess = null 
+
+// newYearCountDown(10) //TypeError: regreess is not a function
+// /*
+// O código causa um erro porque o corpo da função countDown() faz referência ao nome da função countDown que foi definido como null no momento da chamada da função.
+// */
+
+let countDown = function f(fromNumber){
+
+    console.log(fromNumber)
+
+    let nextNumber = fromNumber - 1 
+
     if(nextNumber > 0){
-        regreess(nextNumber) //chamada / return a própria função
+        f(nextNumber)
     }
     else
-    'ERROR' 
-
+    'ERROR'
 }
 
-regreess(4)
+let newYearCountDown = countDown
 
-let newYearCountDown = regreess
-
-regreess = null 
+countDown = null 
 
 newYearCountDown(10)
+
