732b5d1b82415ac5eb75a97000130c029ad237b6
VICTOR DAVI <113daviiicamach@gmail.com>
2023-08-29 20:46:22 -0300

    testando ForEach

---
+++ javascript/teste/ambiente.js
@@ -0,0 +1,16 @@
+let listaDeAprovados = [
+    'ada.lovelce@gmail.com',
+    'camachoVictor@gmail.com',
+    '113daviiicamacho@hotmail.com',
+    'robertoFirme@meteoloko.com'
+];
+
+const enviarEmail = (email) => {
+    console.log(`email para ${email} foi envaido com sucesso!`);  
+};
+
+listaDeAprovados.forEach((item, position, lista) =>{
+    enviarEmail(item);
+    console.log(`Sua posição na fila é: ${position}`)
+    console.log(`A quantidade de pessoas na lista é de: ${lista.length}`)
+});
